30abad917510d77343ad749acdfdb69b402d617b
ChangLiuuu <spiritchang@hotmail.com>
2017-07-04 17:37:23 -0700

    add jquery

---
+++ resources/js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+    $('.js--section-feacture').waypoint(function (direction) {
+        if (direction === "down") {
+            $('nav').addClass('.sticky');
+        } else {
+            $('nav').removeClass('.sticky');
+        }
+    }, {
+        offset: '60px;'
+    });
+
+    // $('h1').click(function () {
+    //     $(this).css('background-color', 'red');
+    // })
+
+
+    /*
+    var waypoint = new Waypoint({
+     element: document.getElementById('px-offset-waypoint'),
+     handler: function(direction) {
+     notify('I am 20px from the top of the window')
+     },
+     offset: 20
+     })
+     **/
+});
+
+
